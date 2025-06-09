0c3a5c8f997ff4853cb9a39e1cdc108645611314
PedroSantana23 <ph.silva.santana@gmail.com>
2025-06-09 17:30:06 -0300

    Correções

--- Agendamento/script.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.getElementById('sidebar');
-
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('open');     // abre/fecha menu
-    menuToggle.classList.toggle('open');  // anima ícone (se quiser)
-});
